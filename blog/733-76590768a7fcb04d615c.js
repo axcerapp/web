43e2ec3b460f3ead931382a09bf2383a2cfceef2
NSavinda <hwnsavi@gmail.com>
2021-11-04 18:06:34 +0530

    blog

---
+++ blog/733-76590768a7fcb04d615c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_elegantstack_gatsby_starter_flexiblog_minimal_v2=self.webpackChunk_elegantstack_gatsby_starter_flexiblog_minimal_v2||[]).push([[733],{733:function(e,t,a){a.r(t);a(7378);var i=a(8945),l=a(6495),n={link:{display:"inline-flex",color:"heading",fontWeight:"medium",":visited":{color:"heading"},svg:{fill:"omegaDarker",height:24,ml:2}}};t.default=function(){return(0,l.tZ)(i.rU,{variant:"mute",target:"_blank",title:"Gatsby",href:"https://www.gatsbyjs.org",rel:"noopener",sx:n.link})}}}]);
+//# sourceMappingURL=733-76590768a7fcb04d615c.js.map
