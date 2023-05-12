6361125e6d5a1983025bf41b668242a2f449e3b9
Thushan96 <thushanmadhulakshitha@gmail.com>
2023-05-12 17:00:13 +0530

    changed portfolio

---
+++ component---src-pages-book-js-4307d8acb91f2bc0164a.js
@@ -0,0 +1,2 @@
+(self.webpackChunkfastland_react_app=self.webpackChunkfastland_react_app||[]).push([[8353],{45698:function(e,n,t){"use strict";t.r(n),t.d(n,{default:function(){return m}});var l=t(67294),a=t(63653),r=t(24943),u=t(24009),c=t(31582),f=t(67974);function m(){return l.createElement(l.Fragment,null,l.createElement(a.FE,{innerPage:!0},l.createElement(r.Z,null),l.createElement(u.Z,null),l.createElement(f.Z,null),l.createElement(c.Z,null)))}}}]);
+//# sourceMappingURL=component---src-pages-book-js-4307d8acb91f2bc0164a.js.map
---
+++ component---src-pages-contact-js-39320e84223b64082cee.js
@@ -0,0 +1,2 @@
+(self.webpackChunkfastland_react_app=self.webpackChunkfastland_react_app||[]).push([[5501],{90262:function(e,n,t){"use strict";t.r(n),t.d(n,{default:function(){return m}});var l=t(67294),a=t(63653),r=t(24943),u=t(24009),c=t(31582),f=t(67974);function m(){return l.createElement(l.Fragment,null,l.createElement(a.FE,{innerPage:!0},l.createElement(r.Z,null),l.createElement(u.Z,null),l.createElement(f.Z,null),l.createElement(c.Z,null)))}}}]);
+//# sourceMappingURL=component---src-pages-contact-js-39320e84223b64082cee.js.map
